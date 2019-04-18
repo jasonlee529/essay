f9c80fa0daed2d3915c8a5fcfaebf3486d8f9717
libo <libo@infisa.cn>
2019-04-18 16:48:31 +0800

    add bd

--- gulpfile.js
+++ gulpfile.js
@@ -45,13 +45,13 @@ gulp.task('minify-js', function() {
 // 在命令行输入 gulp images 启动此任务
 gulp.task('images', function () {
     // 1. 找到图片
-    gulp.src('./public/*.*')
+    gulp.src('./public/images/**')
     // 2. 压缩图片
         .pipe(imagemin({
             progressive: true
         }))
         // 3. 另存图片
-        .pipe(gulp.dest('dist/'))
+        .pipe(gulp.dest('public/images'))
 });
 gulp.task('other', function () {
     // 1. 找到图片
